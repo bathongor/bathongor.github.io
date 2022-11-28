e44ecb5e5b2d49a2769644853fa3fc9464d47385
Batkhongor Chagnaa <ch.bathongor@gmail.com>
2022-11-28 01:51:31 +0100

    added floor texture, steering done

--- scripts/Test.js
+++ scripts/Test.js
@@ -3,6 +3,7 @@ import * as CANNON from "cannon-es";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import CannonDebugger from "cannon-es-debugger";
+import { bodyToMesh } from "./three-conversion-utils.js";
 
 export default class Experience {
   static instance;
@@ -18,35 +19,10 @@ export default class Experience {
     this.initCamControls();
     this.addPlane();
     this.loadGLTF("/car.glb");
-    // this.initPhysics();
-
-    // this.renderer.setAnimationLoop(this.animate);
-
-    this.keyStatus = {
-      keys: {
-        ArrowUp: false,
-        ArrowDown: false,
-        ArrowLeft: false,
-        ArrowRight: false,
-      },
-      isActive: false,
-    };
-
     this.initKeyEventListeners();
   }
 
   initKeyEventListeners = () => {
-    // document.addEventListener('keydown', (event) => {
-    //   if(event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowLeft" || event.key == "ArrowRight"){
-    //     this.keyStatus.keys[event.key] = true;
-    //     this.keyStatus.isActive = true;
-    //   }
-    // });
-
-    // document.addEventListener('keyup', (event) => {
-    //   this.keyStatus.keys[event.key] = false;
-    //   this.keyStatus.isActive = false;
-    // });
     document.addEventListener("keydown", (event) => {
       const maxSteerVal = 0.5;
       const maxForce = 1000;
@@ -162,47 +138,29 @@ export default class Experience {
     this.world = new CANNON.World({
       gravity: new CANNON.Vec3(0, -9.81, 0),
     });
-    this.world.broadphase = new CANNON.SAPBroadphase(this.world);
 
-    const groundBody = new CANNON.Body({
-      type: CANNON.Body.STATIC,
-      shape: new CANNON.Plane(),
-    });
-    groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-    this.world.addBody(groundBody);
-
+    // Build the car chassis
     const approxBox = new THREE.Box3().setFromObject(this.glb);
     const size = approxBox.getSize(new THREE.Vector3());
     size.x /= 2;
     size.y /= 2;
     size.z /= 2;
-    console.log(size);
-
-    // const testBody = new CANNON.Body({
-    //   mass: 5,
-    //   shape: new CANNON.Cylinder(),
-    // });
-    // testBody.position.set(0, 6, 0);
-    // this.world.addBody(testBody);
-
-    // Build the car chassis
-    const chassisBody = new CANNON.Body({
-      mass: 150,
-    });
-    const chassisShape = new CANNON.Box(size);
+    const chassisShape = new CANNON.Box(
+      new CANNON.Vec3(size.y - 0.2, size.z - 0.2, size.x - 0.2)
+    );
+    const chassisBody = new CANNON.Body({ mass: 150 });
     chassisBody.addShape(chassisShape);
-    chassisBody.position.set(0, 7, 0);
-    chassisBody.collisionFilterGroup = 1;
+    chassisBody.position.set(0, 4, 0);
+    // chassisBody.angularVelocity.set(0, 0.5, 0)
 
-    //Create the vehicle
+    // Create the vehicle
     this.vehicle = new CANNON.RaycastVehicle({
       chassisBody,
     });
 
     const wheelOptions = {
-      mass: 1,
-      radius: 0.5,
-      directionLocal: new CANNON.Vec3(0, -2, 0),
+      radius: 0.45,
+      directionLocal: new CANNON.Vec3(0, -1, 0),
       suspensionStiffness: 30,
       suspensionRestLength: 0.3,
       frictionSlip: 1.4,
@@ -210,44 +168,44 @@ export default class Experience {
       dampingCompression: 4.4,
       maxSuspensionForce: 100000,
       rollInfluence: 0.01,
-      axleLocal: new CANNON.Vec3(-1, 0, 0),
-      chassisConnectionPointLocal: new CANNON.Vec3(1, 1, 0),
+      axleLocal: new CANNON.Vec3(0, 0, 1),
+      chassisConnectionPointLocal: new CANNON.Vec3(-1, 0, 1),
       maxSuspensionTravel: 0.3,
       customSlidingRotationalSpeed: -30,
       useCustomSlidingRotationalSpeed: true,
     };
 
     wheelOptions.chassisConnectionPointLocal.set(
-      -size.x - 0.2,
-      0,
-      size.z - 2 * wheelOptions.radius
+      -size.y + 1.6,
+      -0.25,
+      size.x - 0.3
     );
     this.vehicle.addWheel(wheelOptions);
 
     wheelOptions.chassisConnectionPointLocal.set(
-      size.x + 0.2,
-      0,
-      size.z - 2 * wheelOptions.radius
+      -size.y + 1.6,
+      -0.15,
+      -size.x + 0.3
     );
     this.vehicle.addWheel(wheelOptions);
 
     wheelOptions.chassisConnectionPointLocal.set(
-      -size.x - 0.2,
-      0,
-      -size.z + 2 * wheelOptions.radius
+      size.y - 1.2,
+      -0.15,
+      size.x - 0.3
     );
     this.vehicle.addWheel(wheelOptions);
 
     wheelOptions.chassisConnectionPointLocal.set(
-      size.x + 0.2,
-      0,
-      -size.z + 2 * wheelOptions.radius
+      size.y - 1.2,
+      -0.23,
+      -size.x + 0.3
     );
     this.vehicle.addWheel(wheelOptions);
 
     this.vehicle.addToWorld(this.world);
 
-    //Add the wheel bodies
+    // Add the wheel bodies
     const wheelBodies = [];
     const wheelMaterial = new CANNON.Material("wheel");
     this.vehicle.wheelInfos.forEach((wheel) => {
@@ -258,23 +216,22 @@ export default class Experience {
         20
       );
       const wheelBody = new CANNON.Body({
-        mass: 1,
+        mass: 0,
         material: wheelMaterial,
       });
       wheelBody.type = CANNON.Body.KINEMATIC;
-      wheelBody.collisionFilterGroup = 0;
-      // wheelBody.collisionFilterMask = 1;
+      wheelBody.collisionFilterGroup = 0; // turn off collisions
       const quaternion = new CANNON.Quaternion().setFromEuler(
         -Math.PI / 2,
         0,
-        -Math.PI / 2
+        0
       );
       wheelBody.addShape(cylinderShape, new CANNON.Vec3(), quaternion);
       wheelBodies.push(wheelBody);
       this.world.addBody(wheelBody);
     });
 
-    //Update the wheel boies
+    // Update the wheel bodies
     this.world.addEventListener("postStep", () => {
       for (let i = 0; i < this.vehicle.wheelInfos.length; i++) {
         this.vehicle.updateWheelTransform(i);
@@ -285,14 +242,56 @@ export default class Experience {
       }
     });
 
+    // Add the ground
+    const sizeX = 64;
+    const sizeZ = 64;
+    const matrix = [];
+    for (let i = 0; i < sizeX; i++) {
+      matrix.push([]);
+      for (let j = 0; j < sizeZ; j++) {
+        if (i === 0 || i === sizeX - 1 || j === 0 || j === sizeZ - 1) {
+          // const height = 3;
+          matrix[i].push(50);
+          continue;
+        }
+        matrix[i].push(1);
+      }
+    }
+
+    const groundMaterial = new CANNON.Material("ground");
+    const heightfieldShape = new CANNON.Heightfield(matrix, {
+      elementSize: 100 / sizeX,
+    });
+    const heightfieldBody = new CANNON.Body({
+      mass: 0,
+      material: groundMaterial,
+    });
+
+    heightfieldBody.addShape(heightfieldShape);
+    heightfieldBody.position.set(
+      -(sizeX * heightfieldShape.elementSize) / 2,
+      -1,
+      (sizeZ * heightfieldShape.elementSize) / 2
+    );
+    heightfieldBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    this.world.addBody(heightfieldBody);
+
+    // const groundMaterial = new CANNON.Material('ground');
+    // const groundBody = new CANNON.Body({
+    //   type: CANNON.Body.STATIC,
+    //   material: groundMaterial,
+    //   shape: new CANNON.Plane(),
+    // });
+    // groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    // world.addBody(groundBody);
+
     // Define interactions between wheels and ground
-    const groundMaterial = new CANNON.Material("groundMaterial  ");
     const wheel_ground = new CANNON.ContactMaterial(
       wheelMaterial,
       groundMaterial,
       {
         friction: 0.3,
-        restition: 0,
+        restitution: 0,
         contactEquationStiffness: 1000,
       }
     );
@@ -303,6 +302,7 @@ export default class Experience {
 
   loadGLTF = (path) => {
     const loader = new GLTFLoader();
+    console.log("outisde callback", this.vehicle);
     loader.load(
       path,
       (gltf) => {
@@ -311,10 +311,13 @@ export default class Experience {
           if (node.isMesh) node.castShadow = true;
         });
         this.glb.position.y = 5;
+        this.glb.rotateX(Math.PI / 2);
 
         this.scene.add(gltf.scene);
         this.initPhysics();
-        this.renderer.setAnimationLoop(this.animate);
+        console.log("inside callback", this);
+        let boundAnimate = this.animate.bind(this);
+        this.renderer.setAnimationLoop(boundAnimate);
       },
       undefined,
       function (error) {
@@ -325,26 +328,40 @@ export default class Experience {
 
   addPlane = () => {
     const planeGeometry = new THREE.PlaneGeometry(300, 300);
-    const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xf5f5f5 });
+    const planeMaterial = this.createFloorMaterial();
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotateX((3 / 2) * Math.PI);
     plane.receiveShadow = true;
     this.scene.add(plane);
   };
 
+  createFloorMaterial = () => {
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load("/texture.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(16, 16);
+
+    const material = new THREE.MeshStandardMaterial({
+      map: texture,
+    });
+
+    return material;
+  };
+
   animate = () => {
     this.renderer.render(this.scene, this.camera);
 
-    // if(this.glb){
-    //   this.glb.position.copy(this.vehicle.position);
-    //   this.glb.position.y -= 0.85;
-    //   this.glb.quaternion.copy(this.vehicle.quaternion);
-    //   console.log(this.glb.position, this.vehicle.position)
-    // }
-
-    if (this.cannonDebugger) {
-      this.cannonDebugger.update();
+    if (this.glb && this.vehicle) {
+      this.glb.position.copy(this.vehicle.chassisBody.position);
+      this.glb.position.y -= 0.85;
+      this.glb.quaternion.copy(this.vehicle.chassisBody.quaternion);
+      this.glb.rotateY(-Math.PI / 2);
     }
+
+    // if (this.cannonDebugger) {
+    //   this.cannonDebugger.update();
+    // }
 
     console.log(this.world.broadphase);
     this.world.fixedStep();
@@ -352,97 +369,3 @@ export default class Experience {
 }
 
 const experience = new Experience(document.getElementById("threejscanvas"));
-
-// let keyStatus = { keys: {
-//     'ArrowUp': false,
-//     'ArrowDown': false,
-//     'ArrowLeft': false,
-//     'ArrowRight': false
-//   }, isActive: false};
-
-//   document.addEventListener('keydown', (event) => {
-//     if(event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowLeft" || event.key == "ArrowRight"){
-//       keyStatus.keys[event.key] = true;
-//       keyStatus.isActive = true;
-//     }
-//   });
-
-//   document.addEventListener('keyup', (event) => {
-//     keyStatus.keys[event.key] = false;
-//     keyStatus.isActive = false;
-//   });
-
-//   const canvas = document.getElementById("threejscanvas");
-//   const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   renderer.shadowMap.enabled = true;
-//   const scene = new THREE.Scene();
-//   const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-//   const orbit = new OrbitControls(camera, renderer.domElement);
-//   camera.position.set(0, 2, -6);
-//   orbit.update();
-//   const loader = new GLTFLoader();
-
-//   //Light Config
-//   const light = new THREE.PointLight(0xffffff, 1.0, 100);
-//   light.position.set(-20,20,20);
-//   light.castShadow = true;
-//   scene.add(light);
-
-//   light.shadow.mapSize.width = 2048;
-//   light.shadow.mapSize.height = 2048;
-//   light.shadow.camera.near = 0.5;
-//   light.shadow.camera.far = 500;
-
-//   let glb = {};
-//   loader.load("/car.glb", ( gltf )  => {
-//     glb = gltf.scene;
-//     glb.traverse((node) => {
-//       if(node.isMesh)
-//         node.castShadow = true;
-//     })
-//     scene.add( gltf.scene );
-
-//   }, undefined, function ( error ) {
-
-//     console.error( error );
-//   });
-
-//   const planeGeometry = new THREE.PlaneGeometry(300, 300);
-//   const planeMaterial = new THREE.MeshStandardMaterial({color: 0xF5F5F5});
-//   const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-//   plane.rotateX(3/2*Math.PI);
-//   plane.receiveShadow = true;
-//   scene.add(plane);
-
-//   function animate() {
-//       if(keyStatus.isActive){
-//         const up = keyStatus.keys["ArrowUp"];
-//         const down = keyStatus.keys["ArrowDown"];
-//         const left = keyStatus.keys["ArrowLeft"];
-//         const right = keyStatus.keys["ArrowRight"];
-
-//         if(up || down){
-//           if(left){
-//             glb.position.x += 0.1;
-//             if(up)glb.rotateY(0.005);
-//             if(down)glb.rotateY(-0.005);
-//           }
-//           else if(right){
-//             glb.position.x -= 0.1;
-//             if(up)glb.rotateY(-0.005);
-//             if(down)glb.rotateY(0.005);
-//           }
-//           else {
-//             if(up)glb.position.z += 0.1;
-//             if(down)glb.position.z -= 0.1;
-//           }
-//         }
-//       }
-//       renderer.render(scene, camera);
-//   }
-
-//   renderer.setAnimationLoop(animate);
-
-//   const helper = new THREE.CameraHelper(light.shadow.camera);
-//   scene.add(helper);
